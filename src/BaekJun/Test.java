b1dec6558ff80efdb893bd03f8c48ff5925dc9dd
woohyung0511 <erst321@naver.com>
2023-03-26 19:03:06 +0900

    테스트 업로드

---
+++ src/BaekJun/Test.java
@@ -0,0 +1,11 @@
+package BaekJun;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author seulg
+ *
+ */
+module Coding_Test {
+}
