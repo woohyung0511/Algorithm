0ea052ec0245262a89845afab507e01b330a1a82
이우형 <124226476+woohyung0511@users.noreply.github.com>
2023-04-08 23:29:37 +0900

    [Bronze V] Title: 사파리월드, Time: 212 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc=new Scanner(System.in);
+
+		long a,b;
+		
+		a=sc.nextLong();
+		b=sc.nextLong();
+		
+		System.out.println(Math.abs(a-b));
+	}
+
+}
