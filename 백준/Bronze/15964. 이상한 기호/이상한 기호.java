e74ffdb61d4d844a8f2400f0bae859d1a35dc982
이우형 <124226476+woohyung0511@users.noreply.github.com>
2023-04-15 18:09:07 +0900

    [Bronze V] Title: 이상한 기호, Time: 204 ms, Memory: 17636 KB -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		Scanner sc=new Scanner(System.in);
 		
-		int A=sc.nextInt();
-		int B=sc.nextInt();
+		long A=sc.nextLong();
+		long B=sc.nextLong();
 		
 		System.out.println((A+B)*(A-B));
 		
