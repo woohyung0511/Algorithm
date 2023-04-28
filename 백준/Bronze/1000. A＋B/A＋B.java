dc1b61a44c7adce62bedd71d77703374ccd72470
이우형 <124226476+woohyung0511@users.noreply.github.com>
2023-04-28 17:00:47 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
-
+ 
 public class Main {
-
+ 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Scanner sc=new Scanner (System.in);
-		int a,b;
-		a=sc.nextInt();
-		b=sc.nextInt();
-		System.out.println(a+b);
+ 
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+		
+		System.out.println(A+B);
+ 
+		in.close();
 	}
-
 }
