df23736cf477c88fceb4593a4df996fa72ff1d45
이우형 <124226476+woohyung0511@users.noreply.github.com>
2023-04-29 23:52:41 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 208 ms, Memory: 17580 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc=new Scanner(System.in);
+		
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int d=sc.nextInt();
+		int e=sc.nextInt();
+		int f=sc.nextInt();
+		int g=sc.nextInt();
+		
+		c=c-a*b;
+		d=d-a*b;
+		e=e-a*b;
+		f=f-a*b;
+		g=g-a*b;
+		
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		
+	}
+
+}
