e3132cbbc976dd2d81bfa21fc0a1bc8c760d0dee
이우형 <124226476+woohyung0511@users.noreply.github.com>
2023-05-13 22:44:44 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 252 ms, Memory: 19340 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        sc.close();
+        
+        StringBuilder sb = new StringBuilder();
+        int carry = 0; // 올림수를 저장할 변수
+        
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            sb.append(sum % 10);
+            carry = sum / 10;
+        }
+        
+        System.out.println(sb.reverse().toString());
+    }
+
+}
