9521445e05a834f2ab88abd008f61961bb58a5b0
이우형 <124226476+woohyung0511@users.noreply.github.com>
2023-06-09 17:15:15 +0900

    [Bronze II] Title: 저항, Time: 276 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc=new Scanner(System.in);
+		
+		ArrayList<String> list=new ArrayList<>();
+		
+		list.add("black");
+		list.add("brown");
+		list.add("red");
+		list.add("orange");
+		list.add("yellow");
+		list.add("green");
+		list.add("blue");
+		list.add("violet");
+		list.add("grey");
+		list.add("white");	
+		
+		int a=list.indexOf(sc.nextLine())*10;
+		int b=list.indexOf(sc.nextLine());
+		int c=list.indexOf(sc.nextLine());
+		
+		System.out.println((long)((a+b)*Math.pow(10, c)));
+	}
+	
+}
