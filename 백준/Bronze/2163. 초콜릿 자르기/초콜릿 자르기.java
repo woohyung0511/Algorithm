4c98348b3904d3d9393523fb72705963389e2427
이우형 <124226476+woohyung0511@users.noreply.github.com>
2023-06-23 13:24:32 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 208 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc=new Scanner(System.in);
+		
+		int N=sc.nextInt();
+		int M=sc.nextInt();
+		
+		int  a=(N*M)-1;
+		
+		System.out.println(a);
+	}
+
+}
