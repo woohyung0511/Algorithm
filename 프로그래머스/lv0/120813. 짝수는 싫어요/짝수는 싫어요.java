2070f94f8b6cd1c6d42c0300c99ab46f7afc032f
이우형 <124226476+woohyung0511@users.noreply.github.com>
2023-08-12 18:25:55 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,12 +1,12 @@
+import java.util.*;
 
 class Solution {
-    public int[] solution(int n) {
-        int [] answer =new int [(n+1)/2];
-        int a=0;
-        for(int i=1;i<=n;i+=2){
-            answer[a]=i;
-            a++;
+    public List solution(int n) {
+         List<Integer> answerList = new ArrayList<>();
+        
+        for (int i = 1; i <= n; i += 2) {
+            answerList.add(i);
         }
-        return answer;
+        return answerList;
     }
 }
