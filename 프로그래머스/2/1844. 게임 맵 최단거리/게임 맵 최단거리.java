67e3c7fa216f79224f153532235c798bb6547775
이우형 <124226476+woohyung0511@users.noreply.github.com>
2023-10-14 18:39:04 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.32 ms, Memory: 55.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] maps) {
+        int answer = 0;
+        
+        int m = maps.length;
+        int n = maps[0].length;
+        
+        int [][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+        
+        Queue<Node> queue = new LinkedList<>();
+        
+        queue.add(new Node(0,0,1));
+        
+        while(!queue.isEmpty()){
+            Node node = queue.poll();
+            
+            int x = node.x;
+            int y = node.y;
+            int distance = node.distance;
+            
+            if(x == m-1 && y == n-1){
+                return distance;
+            }
+            
+            for(int [] direction : directions){
+                int newX = x + direction[0];
+                int newY = y + direction[1];
+                
+                if(newX >= 0 && newX <= m-1 && newY >= 0 && newY <= n-1 && maps[newX][newY] == 1){
+                    maps[newX][newY] = 0;
+                    queue.add(new Node(newX,newY,distance+1));
+                }
+            }
+        }
+        return -1;
+    }
+    
+    class Node{
+        int x;
+        int y;
+        int distance;
+        
+        public Node(int x,int y,int distance){
+            this.x = x;
+            this.y = y;
+            this.distance = distance;
+        }
+    }
+}
